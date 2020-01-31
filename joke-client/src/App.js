8d13806d1b2f5181c27a357d868fea1b30cc9e93
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2020-01-31 09:35:04 -0800

    Commit 11: set up of private route and how I want the app.js structured

--- joke-client/src/App.js
+++ joke-client/src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Router, Link, Switch } from 'react-router-dom';
+
+import Login from './components/Login';
+import List from './components/List';
+import PrivateRoute from './utils/PrivateRoute';
+
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Log in!</Link>
+          </li>
+          <li>
+            <Link to="/jokes">Joke List 🤡</Link>
+          </li>
+        </ul>
+        <Switch>
+          <PrivateRoute path="/jokes" component={List} />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
